a5b6c7260e6cdce9e872e7004bf5e850605a7ad1
Tigg <ignatovichanastasiia@gmail.com>
2021-08-19 15:51:32 +0300

    testing base, failing tests 5 of 6

--- ThirdLesson/src/test/java/ru/geekbrains/thirdLesson/service/Impl/ProductServiceImplTest.java
+++ ThirdLesson/src/test/java/ru/geekbrains/thirdLesson/service/Impl/ProductServiceImplTest.java
@@ -28,55 +28,52 @@ class ProductServiceImplTest extends SpringMvcDemoApplicationTest {
     Product product1;
     Product product2;
 
+    @BeforeEach
+    void unit() {
+        Product product = new Product(1l, "title", 20000);
+        Product product1 = new Product(2L, "title", 30000);
+    }
+
     @MockBean
     Map<Long, Product> productMapTest;
     Map<Long, Product> productMap;
 
-
-
-    @BeforeEach
-    void setUp() {
-
-        Product product = new Product(1l,"title",20000);
-        Product product1 = new Product(2L,"title", 30000);
-    }
-
     @Test
     void newProductAdd() {
-        productMapTest.put(product.getId(),product);
-        productService.newProductAdd(product.getId(), product.getTitle(),product.getCost());
+        productMapTest.put(1L, product);
+        productService.newProductAdd(1l, "title", 20000);
         productMap = productService.getProducts();
-        Assertions.assertEquals(productMap,productMapTest);
+        Assertions.assertEquals(productMap, productMapTest);
     }
 
     @Test
     void add() {
-        productMapTest.put(product1.getId(),product1);
-        productService.add(product1.getId(), product1);
+        productMapTest.put(2L, product1);
+        productService.add(2L, product1);
         productMap = productService.getProducts();
-        Assertions.assertEquals(productMapTest,productMap);
+        Assertions.assertEquals(productMapTest, productMap);
     }
 
     @Test
     void isId() {
-        Assertions.assertTrue(productService.isId(product.getId()));
+        Assertions.assertTrue(productService.isId(1L));
     }
 
     @Test
     void findById() {
-        product2 = productService.findById(product.getId()).get();
-        Assertions.assertEquals(product,product2);
+        product2 = productService.findById(1L).get();
+        Assertions.assertEquals(product, product2);
     }
 
     @Test
     void getProducts() {
-        Assertions.assertEquals(productMapTest,productService.getProducts());
+        Assertions.assertEquals(productMapTest, productService.getProducts());
     }
 
     @Test
     void removeByID() {
         productMapTest.remove(product);
-        productService.removeByID(product.getId());
+        productService.removeByID(1L);
         Assertions.assertEquals(productMapTest, productService.getProducts());
     }
 }
